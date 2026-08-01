a85c3bf1d10bd8f251a7cffc8747578fd9bbaeee
surajbhavake <surajbhavake2@gmail.com>
2026-08-02 00:00:56 +0530

    chore : added the views for getting the system resources data

--- backend/monitor/views.py
+++ backend/monitor/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+import psutil 
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+
+@api_view(['GET'])
+def system_stats(request):
+    data = {
+        'cpu_percent' : psutil.cpu_percent(interval=1),
+        'ram_percent' : psutil.virtual_memory().percent,
+        'disk_percent': psutil.disk_usage('/').percent,
+        'uptime_seconds' : psutil.boot_time(), 
+    }
+    return Response(data)
 
 # Create your views here.
