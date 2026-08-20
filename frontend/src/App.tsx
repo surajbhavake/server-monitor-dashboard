d189b25e874f4e3cb032b5232302a3f218c5a5fa
surajbhavake <surajbhavake2@gmail.com>
2026-08-20 22:58:28 +0530

    chore : frontend setup done

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+
+  
+  return (
+    <>
+      <div className = 'p-8 text-2xl font-bold text-blue-600'>Hello, Dahsboard</div>
+    </>
+  )
+}
+
+export default App
